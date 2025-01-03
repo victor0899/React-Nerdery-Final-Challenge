9b86835c40b0bd349030872a292e0d6de3e72bdb
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-03 16:34:58 -0600

    refactor(layout): remove container class and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo/client';
-import Sidebar from './components/sidebar/Sidebar'
+import { Sidebar } from './components/sidebar/Sidebar';
 import AppHeader from './components/AppHeader/AppHeader'
-import DashboardCard from './components/Dashboard/DashboardCard'
+import DashboardCard from './components/Dashboard'; 
+
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="min-h-screen bg-neutral-5">
-        <div className="container p-8 mx-auto">
-          <div className="flex flex-col md:flex-row gap-8">
-            <Sidebar />
-            <main className="flex-1 w-full">
-              <AppHeader />
-              <DashboardCard />
-            </main>
-          </div>
+    <div className="min-h-screen bg-neutral-5">
+      <div className="p-8">
+        <div className="flex flex-col md:flex-row gap-8">
+          <Sidebar/>
+          <main className="flex-1 w-full">
+            <AppHeader />
+        <DashboardCard/>
+          </main>
         </div>
       </div>
-    </ApolloProvider>
+    </div>
+  </ApolloProvider>
   )
 }
 
