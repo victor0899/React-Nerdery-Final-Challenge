a9b155ae95ec1aa2b98e42f05a679633fb6d0ea9
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-07 16:31:30 -0600

    feat(constants): add navigation items configuration

---
+++ src/components/sidebar/constants/index.ts
@@ -0,0 +1,16 @@
+import { NavigationItem } from '../types';
+
+export const NAVIGATION_ITEMS: NavigationItem[] = [
+  {
+    id: 'dashboard',
+    href: '/dashboard',
+    icon: 'ri-function-line',
+    label: 'DASHBOARD'
+  },
+  {
+    id: 'tasks',
+    href: '/tasks',
+    icon: 'ri-menu-line',
+    label: 'MY TASK'
+  }
+];
