f66504aa6518fbe4b612ecc0d25326f04605c2ad
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-07 11:39:37 -0600

    refactor(app): add ViewProvider and update component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo/client';
 import { SearchProvider } from './context/searchContext';
+import { ViewProvider } from './context/viewContext';
 import { Sidebar } from './components/sidebar/Sidebar';
-import AppHeader from './components/AppHeader/AppHeader';
+import { AppHeader } from './components/appHeader/appHeader';
 import DashboardCard from './components/Dashboard';
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <SearchProvider>
-        <div className="min-h-screen bg-neutral-5">
-          <div className="p-8">
-            <div className="flex flex-col md:flex-row gap-8">
-              <Sidebar/>
-              <main className="flex-1 w-full">
-                <AppHeader />
-                <DashboardCard/>
-              </main>
+        <ViewProvider>
+          <div className="min-h-screen bg-neutral-5">
+            <div className="p-8">
+              <div className="flex flex-col md:flex-row gap-8">
+                <Sidebar/>
+                <main className="flex-1 w-full">
+                  <AppHeader />
+                  <DashboardCard/>
+                </main>
+              </div>
             </div>
           </div>
-        </div>
+        </ViewProvider>
       </SearchProvider>
     </ApolloProvider>
   )
