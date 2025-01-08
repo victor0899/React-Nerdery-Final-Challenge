9513b4f013dd9790d89876fe49657636a14e9aa2
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-07 18:13:25 -0600

    feat(navigation): add profile and about items to sidebar navigation

--- src/components/sidebar/constants/index.ts
+++ src/components/sidebar/constants/index.ts
@@ -12,5 +12,17 @@ export const NAVIGATION_ITEMS: NavigationItem[] = [
     href: '/tasks',
     icon: 'ri-menu-line',
     label: 'MY TASK'
+  },
+  {
+    id: 'profile',
+    href: '/profile',
+    icon: 'ri-user-line',
+    label: 'PROFILE'
+  },
+  {
+    id: 'about',
+    href: '/about',
+    icon: 'ri-information-line',
+    label: 'ABOUT'
   }
 ];
