e00da8bc327e584d695f39eea4da2ab846a5df01
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-09 08:50:07 -0600

    refactor: refine Task related type definitions

--- src/components/Dashboard/types.ts
+++ src/components/Dashboard/types.ts
@@ -1,23 +1,18 @@
+export type PointEstimate = 'ZERO' | 'ONE' | 'TWO' | 'FOUR' | 'EIGHT';
+export type TaskStatus = 'BACKLOG' | 'TODO' | 'IN_PROGRESS' | 'CANCELLED' | 'DONE';
+export type TaskTag = 'ANDROID' | 'IOS' | 'NODE_JS' | 'RAILS' | 'REACT';
+
 export interface Task {
   id: string;
   name: string;
-  status: string;
+  status: TaskStatus;
   dueDate: string;
-  pointEstimate: string;
+  pointEstimate: PointEstimate;
   assignee: {
+    id: string;
     fullName: string;
-    email: string;
-    avatar: string;
+    email?: string;
+    avatar?: string;
   };
-  tags: string[];
-}
-
-export interface CreateTaskInput {
-  name: string;
-  pointEstimate: 'ZERO' | 'ONE' | 'TWO' | 'FOUR' | 'EIGHT';
-  assigneeId: string;
-  dueDate: string;
-  status: 'BACKLOG' | 'TODO' | 'IN_PROGRESS' | 'DONE' | 'CANCELLED';
-  tags: string[];
-  position: number;
+  tags: TaskTag[];
 }
