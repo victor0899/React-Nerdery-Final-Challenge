d22efdd631e1e347323093248a8582fce94cf65d
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-01-09 19:39:02 -0600

    refactor(toolbar): simplify component structure

--- src/components/common/toolBar.tsx
+++ src/components/common/toolBar.tsx
@@ -4,11 +4,9 @@ import { AddButton } from './AddButton';
 
 const ToolBar: React.FC = () => {
   return (
-    <div className="w-full">
-      <div className="flex justify-between items-center w-full px-6 py-3">
-        <ViewToggle />
-        <AddButton />
-      </div>
+    <div className="flex justify-between items-center w-full">
+      <ViewToggle />
+      <AddButton />
     </div>
   );
 };
